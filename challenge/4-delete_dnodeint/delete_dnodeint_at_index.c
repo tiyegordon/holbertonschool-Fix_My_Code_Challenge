86039df3f4880cbcc87f151c8115a6ebd4edac3b
tiyegordon <tiye.gordon@gmail.com>
2026-06-07 15:36:26 -0400

    updated

--- challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
+++ challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
@@ -33,10 +33,9 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	{
 		/* Fix: was (*head)->prev->prev->prev = (*head)->prev; */
 		(*head)->prev->next->next = (*head)->next;
-		tmp = (*head)->next;
-		(*head)->prev->next = tmp;
-		if (tmp)
-			tmp->prev = (*head)->prev;
+		(*head)->prev->next = (*head)->next;
+		if ((*head)->next)
+			(*head)->next->prev = (*head)->prev;
 		free(*head);
 		*head = saved_head;
 	}
