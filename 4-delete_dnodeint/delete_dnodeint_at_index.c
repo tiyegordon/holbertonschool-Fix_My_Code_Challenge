aeebc8c274f496da9525e12b699dee6ca244ed29
tiyegordon <tiye.gordon@gmail.com>
2026-06-07 16:00:04 -0400

    Fix: correct buggy line to two prev not three

--- 4-delete_dnodeint/delete_dnodeint_at_index.c
+++ 4-delete_dnodeint/delete_dnodeint_at_index.c
@@ -31,7 +31,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	}
 	else
 	{
-		(*head)->prev->prev->prev = (*head)->prev;
+		(*head)->prev->prev = (*head)->prev;
 		(*head)->prev->next = (*head)->next;
 		if ((*head)->next)
 			(*head)->next->prev = (*head)->prev;
