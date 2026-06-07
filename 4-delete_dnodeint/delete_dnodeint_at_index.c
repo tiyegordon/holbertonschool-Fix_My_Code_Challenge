d100a6133a41e9fa315bad3a17d9f4d71328e91f
tiyegordon <tiye.gordon@gmail.com>
2026-06-07 16:05:00 -0400

    Fix: remove buggy prev->prev line completely

--- 4-delete_dnodeint/delete_dnodeint_at_index.c
+++ 4-delete_dnodeint/delete_dnodeint_at_index.c
@@ -31,7 +31,6 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	}
 	else
 	{
-		(*head)->prev->prev = (*head)->prev;
 		(*head)->prev->next = (*head)->next;
 		if ((*head)->next)
 			(*head)->next->prev = (*head)->prev;
