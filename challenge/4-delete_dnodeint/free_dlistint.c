657a493f71e07cb44118930bba408d1871d93944
tiyegordon <tiye.gordon@gmail.com>
2026-06-07 15:34:05 -0400

    Add missing C files to 4-delete_dnodeint

---
+++ challenge/4-delete_dnodeint/free_dlistint.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include "lists.h"
+
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *node;
+
+	while (head)
+	{
+		node = head;
+		head = head->next;
+		free(node);
+	}
+}
