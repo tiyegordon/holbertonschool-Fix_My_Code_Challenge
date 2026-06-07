77aebec16249cde761b34f1512339fcdb193e22c
tiyegordon <tiye.gordon@gmail.com>
2026-06-07 15:57:31 -0400

    Fix: prev->prev to prev->next

--- 4-delete_dnodeint/delete_dnodeint_at_index.c
+++ 4-delete_dnodeint/delete_dnodeint_at_index.c
@@ -32,7 +32,6 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	else
 	{
 		(*head)->prev->prev->prev = (*head)->prev;
-		(*head)->prev->next->next = (*head)->next;
 		(*head)->prev->next = (*head)->next;
 		if ((*head)->next)
 			(*head)->next->prev = (*head)->prev;
