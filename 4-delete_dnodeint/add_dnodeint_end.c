770cc33c3d24ada14d02480eb87b42c11d79c077
tiyegordon <tiye.gordon@gmail.com>
2026-06-07 15:40:10 -0400

    Move 4-delete_dnodeint to root of repo

---
+++ 4-delete_dnodeint/add_dnodeint_end.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include "lists.h"
+
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+	dlistint_t *new;
+	dlistint_t *l;
+
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = n;
+	new->next = NULL;
+	if (*head == NULL)
+	{
+		*head = new;
+		new->prev = NULL;
+		return (new);
+	}
+	l = *head;
+	while (l->next != NULL)
+		l = l->next;
+	l->next = new;
+	new->prev = l;
+	return (new);
+}
---
+++ 4-delete_dnodeint/print_dlistint.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "lists.h"
+
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t n;
+
+	n = 0;
+	while (h)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		n++;
+	}
+	return (n);
+}
